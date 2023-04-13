1de9cf696a756e5b35dd8d0f704e73bae20c78ba
ll-zhongyuan <1614294536@qq.com>
2023-04-14 01:12:12 +0800

    ui组件二次封装

---
+++ fore-end-demo/Element-ui组件二次封装/MyInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="my-input">
+    <el-input ref="inp" v-bind="$attrs">
+      <template v-for="(value, name) in $slots" #[name]="slotData">
+        <slot :name="name" v-bind="slotData || {}"></slot>
+      </template>
+    </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  //   props: ["a", "b"],
+  mounted() {
+    const entries = Object.entries(this.$refs.inp);
+    for (const [key, value] of entries) {
+      this[key] = value;
+    }
+  },
+  created() {
+    console.log(this.$attrs);
+    console.log(this.$slots);
+    console.log(this.$refs.inp);
+  },
+};
+</script>
+
+<style scoped>
+.my-input {
+  transition: 0.3s;
+}
+.my-input:hover,
+.my-input:focus-within {
+  fill: drop-shadow(0 0 3px rgb(0, 0, 0));
+}
+</style>
---
+++ fore-end-demo/Element-ui组件二次封装/父组件.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <MyInput ref="inputRef" v-model="data">
+      <template #prepend>
+        <el-select placeholder="Select" style="width: 115px;">
+          <el-option label="Restaurant" value="1"></el-option> 
+          <el-option label="Order No." value="2"></el-option>
+          <el-option label="Tel" value="3"></el-option>
+        </el-select>
+      </template>
+      <template #append>
+        <el-button :icon="Search"></el-button>
+      </template>
+    </MyInput>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { MyInput } from "./MyInput.vue";
+import {Search} from "2element-plus/icons-vue"
+const data = ref("");
+const inputRef = ref(null)
+
+onMounted(()=>{
+    console.log(inputRef.value);
+    inputRef.value.focus()
+})
+</script>
